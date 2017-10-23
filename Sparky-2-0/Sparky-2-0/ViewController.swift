3a2b2512e93e4b0e021cc1093491841026c333b4
SergioASU <sicorral@asu.edu>
2017-10-23 13:37:31 -0700

    Initial commit. Blank project

---
+++ Sparky-2-0/Sparky-2-0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sparky-2-0
+//
+//  Created by Sergio Corral on 10/23/17.
+//  Copyright © 2017 Sergio Corral. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
