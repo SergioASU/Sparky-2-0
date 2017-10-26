a52f326c55462983ca0ea46e1cc816bd7bcf738e
SergioASU <sicorral@asu.edu>
2017-10-26 15:58:46 -0700

    Initialized Firebase for the project

---
+++ Sparky-2-0/Sparky-2-0/WaterStation+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  WaterStation+CoreDataClass.swift
+//  Sparky-2-0
+//
+//  Created by Sergio Corral on 10/26/17.
+//  Copyright © 2017 Sergio Corral. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class WaterStation: NSManagedObject {
+
+}
